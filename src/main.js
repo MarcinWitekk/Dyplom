a4d39124a18926c2d3e82c3ffdf42f9c5eeee88c
Marcin Witkowski <marcinwitekpl@outlook.com>
2021-02-27 12:57:19 +0100

    dodawanie i wyswietlanie

--- src/main.js
+++ src/main.js
@@ -3,11 +3,32 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import VueAxios from "vue-axios"
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDwCwqeShHe4OSqLC17vkF1haz3Up3O2k4",
+  authDomain: "rezerwacje-24580.firebaseapp.com",
+  databaseURL: "https://rezerwacje-24580-default-rtdb.firebaseio.com",
+  projectId: "rezerwacje-24580",
+  storageBucket: "rezerwacje-24580.appspot.com",
+  messagingSenderId: "874565189812",
+  appId: "1:874565189812:web:8beda69776bb962e0417e1",
+  measurementId: "G-G8ZE0SNV4K"
+});
+
+export const db = firebase.firestore();
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Start.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+      <Calendar/>     
+  </div>
+</template>
+<script>
+import Calendar from '../components/Calendar';
+
+export default {
+  name: 'Home',
+  components: {
+    Calendar
+  }
+}
+</script>
